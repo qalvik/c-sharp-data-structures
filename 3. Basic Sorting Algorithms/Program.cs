edaefa9b7cf929abca5d59be9e82b3da938e0438
qalvik <46355180+qalvik@users.noreply.github.com>
2020-11-23 21:44:32 +0100

    Bubble sort

---
+++ 3. Basic Sorting Algorithms/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _3._Basic_Sorting_Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "twój stary pijany chleje harnasie pod sklepem";
+
+            var splitted = text.Split(" ");
+
+            Sorting sorting = new Sorting();
+            sorting.BubbleSort(splitted);
+
+        }
+    }
+
+    public class Sorting
+    {
+        public void BubbleSort(string [] arr)
+        {          
+            string temp = string.Empty;
+
+            for (int outer = arr.Length-1;  outer >= 1 ; outer--)
+            {
+                for (int inner = 0; inner <= outer - 1;  inner++)
+                {
+                    if(arr[inner].CompareTo(arr[inner + 1]) > 0 ) 
+                        {
+                            temp = arr[inner];
+                            arr[inner] = arr[inner + 1];
+                            arr[inner + 1] = temp;
+                        }
+                }
+                ShowSortingProcess(arr);
+            }
+
+             
+        }
+
+        public void ShowSortingProcess(string [] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine("\n");
+        } 
+    }
+}
+
+
